4be6b0681538afac90c4563d9427f7f347652b01
Prado Domercq <prado.domercq@gmail.com>
2023-11-06 11:10:47 +0100

    Included mass and number fraction results

--- script_UTOPIA.py
+++ script_UTOPIA.py
@@ -3,15 +3,16 @@ import os
 import pandas as pd
 from datetime import datetime
 
+
 from functions import create_inputsTable_UTOPIA
 from functions.create_rateConstants_tabel import *
 from functions.fillInteractions_df_fun_OOP import *
-from functions.generate_modelObjects import*
+from functions.generate_modelObjects import *
 from functions.generateRateConstants_particles import *
-from functions.solver_SteadyState import*
-from functions.extract_results import*
-from functions.plot_results import*
-from functions.massBalance import*
+from functions.solver_SteadyState import *
+from functions.extract_results import *
+from functions.plot_results import *
+from functions.massBalance import *
 
 inputs_path = os.path.join(os.path.dirname(__file__), "inputs")
 
@@ -27,14 +28,30 @@ current_date = datetime.now().strftime("%Y-%m-%d")
 ## choose input files to load
 
 comp_impFile_name = "\inputs_compartments.csv"
-comp_interactFile_name="\compartment_interactions.csv"
-mp_imputFile_name="\inputs_microplastics.csv"
+comp_interactFile_name = "\compartment_interactions.csv"
+mp_imputFile_name = "\inputs_microplastics.csv"
 
-boxName="Utopia"
+boxName = "Utopia"
 
-runName="LowDensityMP"
+runName = "LowDensityMP"
 
-modelBoxes, system_particle_object_list, SpeciesList, process_inputs_df,spm, dict_comp,model_lists,particles_df,MPforms_list = generate_objects(inputs_path,boxName=boxName,comp_impFile_name=comp_impFile_name, comp_interactFile_name=comp_interactFile_name, mp_imputFile_name=mp_imputFile_name)
+(
+    modelBoxes,
+    system_particle_object_list,
+    SpeciesList,
+    process_inputs_df,
+    spm,
+    dict_comp,
+    model_lists,
+    particles_df,
+    MPforms_list,
+) = generate_objects(
+    inputs_path,
+    boxName=boxName,
+    comp_impFile_name=comp_impFile_name,
+    comp_interactFile_name=comp_interactFile_name,
+    mp_imputFile_name=mp_imputFile_name,
+)
 
 
 """Estimate rate constants per particle"""
@@ -45,7 +62,7 @@ for particle in system_particle_object_list:
 #### Original results with no time limit
 
 ## create rate constants table:
-RC_df= create_rateConstants_table(system_particle_object_list)
+RC_df = create_rateConstants_table(system_particle_object_list)
 df4 = RC_df.fillna(0)
 
 # Plot rate constants
@@ -55,142 +72,192 @@ plot_rate_constants(df4)
 
 interactions_df = fillInteractions_fun_OOP(system_particle_object_list, SpeciesList)
 
-from functions.fill_interactions_Knames import*
+from functions.fill_interactions_Knames import *
 
-interactions_df_Knames=fillInteractions_Knames(system_particle_object_list,SpeciesList
+interactions_df_Knames = fillInteractions_Knames(
+    system_particle_object_list, SpeciesList
 )
 
 """SOLVE SYSTEM OF ODES"""
 
-# Choose input flow (in g per second) 
+# Choose input flow (in g per second)
 # Define particle imput (sp_imput):
 
 # Size fraction:
-    # a= 0.5 um
-    # b= 5 um
-    # c= 50 um
-    # d= 500 um
-    # e= 5000 um
-size_dict=dict(zip(["a","b","c","d","e"],[0.5,5,50,500,5000]))
+# a= 0.5 um
+# b= 5 um
+# c= 50 um
+# d= 500 um
+# e= 5000 um
+size_dict = dict(zip(["a", "b", "c", "d", "e"], [0.5, 5, 50, 500, 5000]))
 # Aggregation state:
-    # A= Free MP
-    # B= heteroaggregatedMP
-    # C= biofouled MP
-    # D= biofouled and heteroaggregated MP
+# A= Free MP
+# B= heteroaggregatedMP
+# C= biofouled MP
+# D= biofouled and heteroaggregated MP
 particle_forms_coding = dict(zip(MPforms_list, ["A", "B", "C", "D"]))
 
-MP_form_dict_reverse={v: k for k, v in particle_forms_coding.items()}
+MP_form_dict_reverse = {v: k for k, v in particle_forms_coding.items()}
 
 # Compartment:
-    # 0= 'Ocean_Surface_Water'
-    # 1= 'Ocean_Mixed_Water'
-    # 2= 'Ocean_Column_Water'
-    # 3= 'Coast_Surface_Water'
-    # 4= 'Coast_Column_Water'
-    # 5= 'Surface_Freshwater'
-    # 6= 'Bulk_Freshwater'
-    # 7= 'Sediment'
-    # 8= 'Urban_Soil_Surface'
-    # 9= 'Urban_Soil'
-    # 10= 'Background_Soil_Surface'
-    # 11= 'Background_Soil'
-    # 12= 'Agricultural_Soil_Surface'
-    # 13= 'Agricultural_Soil'
-    # 14= 'Air'
+# 0= 'Ocean_Surface_Water'
+# 1= 'Ocean_Mixed_Water'
+# 2= 'Ocean_Column_Water'
+# 3= 'Coast_Surface_Water'
+# 4= 'Coast_Column_Water'
+# 5= 'Surface_Freshwater'
+# 6= 'Bulk_Freshwater'
+# 7= 'Sediment'
+# 8= 'Urban_Soil_Surface'
+# 9= 'Urban_Soil'
+# 10= 'Background_Soil_Surface'
+# 11= 'Background_Soil'
+# 12= 'Agricultural_Soil_Surface'
+# 13= 'Agricultural_Soil'
+# 14= 'Air'
 particle_compartmentCoding = dict(
-    zip(model_lists["compartmentNames_list"], list(range(len(model_lists["compartmentNames_list"]))))
+    zip(
+        model_lists["compartmentNames_list"],
+        list(range(len(model_lists["compartmentNames_list"]))),
+    )
 )
 comp_dict_inverse = {v: k for k, v in particle_compartmentCoding.items()}
 
 # input flow (in g per second)
-q_mass_g_s=1
+q_mass_g_s = 1
 
 # particle imput
-size_bin= "e"
-comp="Ocean_Surface_Water"
-MP_form="freeMP"
+size_bin = "e"
+comp = "Ocean_Surface_Water"
+MP_form = "freeMP"
 
-sp_imput=size_bin+particle_forms_coding[MP_form]+str(particle_compartmentCoding[comp])+"_"+boxName
+sp_imput = (
+    size_bin
+    + particle_forms_coding[MP_form]
+    + str(particle_compartmentCoding[comp])
+    + "_"
+    + boxName
+)
 
-print("Imput flow = "+str(q_mass_g_s) +" g/s of "+ MP_form + " into the "+ comp +" compartment" )
+print(
+    "Imput flow = "
+    + str(q_mass_g_s)
+    + " g/s of "
+    + MP_form
+    + " into the "
+    + comp
+    + " compartment"
+)
 
-R = solve_ODES_SS(system_particle_object_list=system_particle_object_list,q_mass_g_s=q_mass_g_s,q_num_s=0,sp_imput=sp_imput,interactions_df=interactions_df)
+R = solve_ODES_SS(
+    system_particle_object_list=system_particle_object_list,
+    q_mass_g_s=q_mass_g_s,
+    q_num_s=0,
+    sp_imput=sp_imput,
+    interactions_df=interactions_df,
+)
 
-#Reformat results (R) dataframe
-R["Size_Fraction_um"]=[size_dict[x[0]] for x in R.index]
-R["MP_Form"]=[MP_form_dict_reverse[x[1]] for x in R.index]
-R["Compartment"]=[comp_dict_inverse[float(x[2:-7])] for x in R.index]
+# Reformat results (R) dataframe
+R["Size_Fraction_um"] = [size_dict[x[0]] for x in R.index]
+R["MP_Form"] = [MP_form_dict_reverse[x[1]] for x in R.index]
+R["Compartment"] = [comp_dict_inverse[float(x[2:-7])] for x in R.index]
 
-Results=R[['Compartment','MP_Form','Size_Fraction_um','mass_g', 'number_of_particles', 'concentration_g_m3',
-       'concentration_num_m3']]
+Results = R[
+    [
+        "Compartment",
+        "MP_Form",
+        "Size_Fraction_um",
+        "mass_g",
+        "number_of_particles",
+        "concentration_g_m3",
+        "concentration_num_m3",
+    ]
+]
 
-#Solve mass balance and print result
-massBalance(R,system_particle_object_list, q_mass_g_s)
+# Solve mass balance and print result
+massBalance(R, system_particle_object_list, q_mass_g_s)
 
 
-Results_comp_dict=extract_by_comp(R.reset_index(),particle_compartmentCoding)
-Results_comp_organiced=extract_by_aggSt(Results_comp_dict,particle_forms_coding)
+Results_comp_dict = extract_by_comp(R.reset_index(), particle_compartmentCoding)
+Results_comp_organiced = extract_by_aggSt(Results_comp_dict, particle_forms_coding)
 
 # Test that there are no negative results
-for i,idx in zip(R["mass_g"],R.index):
-    if i<0:
+for i, idx in zip(R["mass_g"], R.index):
+    if i < 0:
         print("negative values in the solution for " + idx)
     else:
         pass
 
 
-#Plot results in Total number of particles and Total mass
+# Plot results in Total number of particles and Total mass
 for comp in Results_comp_organiced:
-    plot_bySize_total_number_particles(Results_comp_organiced,comp,model_lists["dict_size_coding"])
-    plot_bySize_total_mass(results_dict=Results_comp_organiced,comp_name=comp,dict_size_coding=model_lists["dict_size_coding"])
+    plot_bySize_total_number_particles(
+        Results_comp_organiced, comp, model_lists["dict_size_coding"]
+    )
+    plot_bySize_total_mass(
+        results_dict=Results_comp_organiced,
+        comp_name=comp,
+        dict_size_coding=model_lists["dict_size_coding"],
+    )
 
 
 # Estimate mass and number fractions and extract ranking table of the species with higest fractions to understan the distribution of the particles in the system
 
-Results_extended=estimate_fractions(Results)
+Results_extended = estimate_fractions(Results)
 
-    
-    
+
 """ Save results """
 
 # Create directory with current date
 directory = current_date
 
-path= os.path.join(outputs_path, directory) 
+path = os.path.join(outputs_path, directory)
 
 # check whether directory already exists
 if not os.path.exists(path):
-  os.mkdir(path)
-  print("Folder %s created!" % path)
+    os.mkdir(path)
+    print("Folder %s created!" % path)
 else:
-  print("Folder %s already exists" % path)
-  
+    print("Folder %s already exists" % path)
+
 # Save results in directory
 
 ## Particle properties
-particles_df.to_csv(os.path.join(path,"particle_properties_"+runName+"_"+current_date+".csv"))
+particles_df.to_csv(
+    os.path.join(path, "particle_properties_" + runName + "_" + current_date + ".csv")
+)
 
 ## Process inputs
-process_inputs_df.to_csv(os.path.join(path,"process_inputs_"+runName+"_"+current_date+".csv"))
+process_inputs_df.to_csv(
+    os.path.join(path, "process_inputs_" + runName + "_" + current_date + ".csv")
+)
 
 ## Rate constants
-df4.to_csv(os.path.join(path,"rate_constants_"+runName+"_"+current_date+".csv"))
+df4.to_csv(
+    os.path.join(path, "rate_constants_" + runName + "_" + current_date + ".csv")
+)
 
 ## Interactions matrix
-interactions_df.to_csv(os.path.join(path,"interactions_matrix_"+runName+"_"+current_date+".csv"))
-interactions_df_Knames.to_csv(os.path.join(path,"interactions_matrix_Knames_"+runName+"_"+current_date+".csv"))
+interactions_df.to_csv(
+    os.path.join(path, "interactions_matrix_" + runName + "_" + current_date + ".csv")
+)
+interactions_df_Knames.to_csv(
+    os.path.join(
+        path, "interactions_matrix_Knames_" + runName + "_" + current_date + ".csv"
+    )
+)
 
 ## Results at steady state
 
-Results.to_csv(os.path.join(path,"Results_SS_"+runName+"_"+current_date+".csv"))
-    
-
+Results_extended.to_csv(
+    os.path.join(path, "Results_SS_" + runName + "_" + current_date + ".csv")
+)
 
 
 ###Modify rate constants by stablishing a time limit or chaging specific rate constant values using the change_RC_value function
 # "Timelimit" mode sets up a time limit of 30min on the processes that exceeds that speed (k > 0.000556), while "raw" mode leaves the rate constant as calcualted. The raw version can straing the solver due to time.
 
-#particles_updated= change_RC_value(system_particle_object_list,#rc_name="k_sediment_resuspension",rc_val=1E-7)
+# particles_updated= change_RC_value(system_particle_object_list,#rc_name="k_sediment_resuspension",rc_val=1E-7)
 
 # particles_updated = timeLimit_particles_RC(system_particle_object_list,0.000556)
 
